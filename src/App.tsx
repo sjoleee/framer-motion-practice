8f44575c7626ac75a15c140c89974416dc1eb3b0
sjoleee <cent7425@gmail.com>
2022-08-25 03:18:09 +0900

    feat: :sparkles: variants의 기본 사용법, staggerChildren 학습

--- src/App.tsx
+++ src/App.tsx
@@ -16,16 +16,50 @@ const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   border-radius: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
 `;
+
+const Circle = styled(motion.div)`
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  background-color: #88c4db;
+  margin: 0 10px;
+`;
+
+const boxVariants = {
+  start: { opacity: 0, scale: 0.5 },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      delay: 0.5,
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const circleVariants = {
+  start: { opacity: 0, y: 10 },
+  end: { opacity: 1, y: 0, transition: { type: "spring" } },
+};
 
 function App() {
   return (
     <Wrapper>
-      <Box
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateZ: 360 }}
-        transition={{ type: "spring", bounce: 0.5, duration: 1, delay: 0.5 }}
-      />
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
